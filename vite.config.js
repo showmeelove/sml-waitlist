eb1f1ee7983c7ae8517d237782e1f68647497a0a
VickyJay <victoluolatunji@gmail.com>
2023-05-13 01:51:56 +0100

    fix: added fit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 2000,
-  }
+  // server: {
+  //   port: 2000,
+  // }
 });
