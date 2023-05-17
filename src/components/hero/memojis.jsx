d9fedbfc141a39ff48f64a3b2fc5261101e0155e
dev-phantom <godsfavour1975@gmail.com>
2023-05-17 15:24:34 +0100

    fix: fixed the animation on the landing page

--- src/components/hero/memojis.jsx
+++ src/components/hero/memojis.jsx
@@ -13,7 +13,7 @@ const memoji = () => {
     return (
       <button
         type={type}
-        className="w-[220px] py-[0.125rem] px-[0.5em] bg-transparent border-[1px] border-[#A3A3A3] text-white text-center rounded-[50px] flex items-center  gap-x-5"
+        className="w-[220px] py-[0.125rem] px-[1px] bg-transparent border-[1px] border-[#A3A3A3] text-white text-center rounded-[50px] flex items-center  gap-x-5"
       >
         {icon}
         {children}
@@ -23,50 +23,62 @@ const memoji = () => {
   };
 
   return (
-    <marquee
-      behavior="alternte"
-      scrolldelay="0.1"
-      scrollamount="17"
-      direction="left"
-    >
-      <div className="mt-[30px] flex gap-x-[2rem] z-40 md:mt-[37px] hero-animation">
-        <ButtonWithImg className={"ml-[10px]"} text={"developers"}>
-          <div className="w-[50px]">
-            <img src={icon1} alt="creators" className="w-[100%]" />
-          </div>
-        </ButtonWithImg>
-        <ButtonWithImg text={"writers"}>
-          <div className="w-[50px]">
-            <img src={icon2} alt="/" className="w-[100%]" />
-          </div>
-        </ButtonWithImg>
-        <ButtonWithImg text={"podcasters"}>
-          <div className="w-[50px]">
-            <img src={icon3} alt="creators" className="w-[100%]" />
-          </div>
-        </ButtonWithImg>
-        <ButtonWithImg text={"comedians"}>
-          <div className="w-[50px]">
-            <img src={icon4} alt="creators" className="w-[100%]" />
-          </div>
-        </ButtonWithImg>
-        <ButtonWithImg text={"community"}>
-          <div className="w-[50px]">
-            <img src={icon5} alt="creators" className="w-[100%]" />
-          </div>
-        </ButtonWithImg>
-        <ButtonWithImg text={"musicians"}>
-          <div className="w-[50px]">
-            <img src={icon6} alt="creators" className="w-[100%]" />
-          </div>
-        </ButtonWithImg>
-        <ButtonWithImg text={"creators"}>
-          <div className="w-[50px]">
-            <img src={icon7} alt="creators" className="w-[100%]" />
-          </div>
-        </ButtonWithImg>
+<div className="mt-[30px] mx-3 px-5 flex gap-x-[2rem] z-40 md:mt-[37px] hero-animation overflow-hidden">
+  <div className="animate-alternate">
+    <ButtonWithImg text="developers">
+      <div className="w-[50px]">
+        <img src={icon1} alt="creators" className="w-[100%]" />
       </div>
-    </marquee>
+    </ButtonWithImg>
+  </div>
+  <div className="animate-alternate">
+    <ButtonWithImg text="writers">
+      <div className="w-[50px]">
+        <img src={icon2} alt="/" className="w-[100%]" />
+      </div>
+    </ButtonWithImg>
+  </div>
+  <div className="animate-alternate">
+    <ButtonWithImg text="podcasters">
+      <div className="w-[50px]">
+        <img src={icon3} alt="creators" className="w-[100%]" />
+      </div>
+    </ButtonWithImg>
+  </div>
+  <div className="animate-alternate">
+    <ButtonWithImg text="comedians">
+      <div className="w-[50px]">
+        <img src={icon4} alt="creators" className="w-[100%]" />
+      </div>
+    </ButtonWithImg>
+  </div>
+  <div className="animate-alternate">
+    <ButtonWithImg text="community">
+      <div className="w-[50px]">
+        <img src={icon5} alt="creators" className="w-[100%]" />
+      </div>
+    </ButtonWithImg>
+  </div>
+  <div className="animate-alternate">
+    <ButtonWithImg text="musicians">
+      <div className="w-[50px]">
+        <img src={icon6} alt="creators" className="w-[100%]" />
+      </div>
+    </ButtonWithImg>
+  </div>
+  <div className="animate-alternate">
+    <ButtonWithImg text="creators">
+      <div className="w-[50px]">
+        <img src={icon7} alt="creators" className="w-[100%]" />
+      </div>
+    </ButtonWithImg>
+  </div>
+
+  <style>{`
+
+  `}</style>
+</div>
+
   );
 };
 
